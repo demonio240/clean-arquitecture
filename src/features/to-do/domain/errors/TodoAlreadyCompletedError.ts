ec827f9a5ede68526f10aa7bb39787ce840e250b
demonio240 <ramirea126y6@gmail.com>
2026-01-10 17:40:07 -0600

    Update: removed TodoAlreadyCompletedError.ts

--- src/features/to-do/domain/errors/TodoAlreadyCompletedError.ts
+++
@@ -1,9 +0,0 @@
-import type { TodoId } from '../value-objects/TodoId';
-import { DomainError } from './DomainError';
-
-export class TodoAlreadyCompletedError extends DomainError {
-  constructor(todoId: TodoId) {
-    super(`El Todo con ID ${todoId.value} ya está completado y no puede completarse de nuevo.`);
-    
-  }
-}
